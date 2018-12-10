7584aed2f05bbc3cfb9dae1cfbff705d52fda99c
Roman Tarasiuk <roman.tarasiuk.l@gmail.com>
2018-12-10 11:32:29 +0200

    Renamed title and components/properties.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-hello-world'`, () => {
+  it(`should have as title 'notes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-hello-world');
+    expect(app.title).toEqual('notes');
   });
 
   it('should render title in a h1 tag', () => {
